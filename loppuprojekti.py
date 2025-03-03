24420404d236e2fe754c7d08090752a43f7ac110
Mika Lohi <152972334+Moti555@users.noreply.github.com>
2025-03-03 14:25:27 +0200

    viimeinen silaus

--- loppuprojekti.py
+++ loppuprojekti.py
@@ -9,7 +9,7 @@ import scipy.signal as signal
 from scipy.fft import fft, fftfreq
 from math import radians, sin, cos, sqrt, asin
 
-urlacc = "https://github.com/Moti555/fymalopputehtava/blob/main/Linear%20Accelerometer.csv"
+urlacc = "https://raw.githubusercontent.com/Moti555/fymalopputehtava/refs/heads/main/Linear%20Accelerometer.csv"
 df = pd.read_csv(urlacc)
 
 
@@ -20,42 +20,41 @@ def butter_lowpass_filter(data, cutoff, fs, nyq, order):
     return y
 
 
-T = df["Time (s)"][len(df["Time (s)"]) - 1] - df["Time (s)"][0] #Koko datan pituus
-n = len(df["Time (s)"]) #Datanäytteiden lukumäärä
-fs = n / T  #Näytteenottotaajuus (olettaen vakioksi)
-nyq = fs / 2  #Nyqvistin taajuus
-order = 3  #Kertaluku
-cutoff = 1 / (0.2)  #Cut-off taajuus
+T = df["Time (s)"][len(df["Time (s)"]) - 1] - df["Time (s)"][0] 
+n = len(df["Time (s)"]) 
+fs = n / T  
+nyq = fs / 2  
+cutoff = 1 / (0.2)  
 
 df["filter_a_x"] = butter_lowpass_filter(df["X (m/s^2)"], cutoff, fs, nyq, order)
 
-# askelmäärä huiput ja laaksot
+
 acc = df["filter_a_x"]
 peaks, _ = signal.find_peaks(acc, height=0)
 valleys, _ = signal.find_peaks(-acc, height=0)
 steps = len(peaks)
 
-# Askeleiden määrä Fourier
+
 acc_fft = fft(acc)
 frequencies = fftfreq(len(acc), 1/fs)
 magnitude = np.abs(acc_fft)
 dominant_frequency = frequencies[np.argmax(magnitude[1:]) + 1]
 steps_fft = int(dominant_frequency * T)
 
-#Havertsinen kaava pisteiden välimatkojen laskemiseen
-R = 6371  # Maapallon säde
+
+R = 6371  
 def haversine(lon1, lat1, lon2, lat2):
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
     c = 2 * asin(sqrt(a))
-    return c * R * 1000  # metreiksi
-urlloc = "https://github.com/Moti555/fymalopputehtava/blob/main/Location.csv"
+    return c * R * 1000  
+urlloc = "https://raw.githubusercontent.com/Moti555/fymalopputehtava/refs/heads/main/Location.csv"
 df_loc = pd.read_csv(urlloc)
 df_loc["dist"] = np.zeros(len(df_loc))
 df_loc["time_diff"] = np.zeros(len(df_loc))
-#Lasketaan nopeus ja matka
+
 for i in range(len(df_loc) - 1):
     df_loc.loc[i, "dist"] = haversine(df_loc.loc[i, "Longitude (°)"], df_loc.loc[i, "Latitude (°)"],
     df_loc.loc[i+1, "Longitude (°)"], df_loc.loc[i+1, "Latitude (°)"])
@@ -70,16 +69,14 @@ total_time = df_loc["Time (s)"].iloc[-1] - df_loc["Time (s)"].iloc[0]
 average_speed = total_distance / total_time
 average_speed_kmh = average_speed * 3.6
 
-step_count = 779  # Suodatettujen askelten määrä
+step_count = 779  
 step_length = total_distance / step_count if step_count > 0 else 0
 
-# Kartta
 start_lat = df_loc["Latitude (°)"].mean()
 start_long = df_loc["Longitude (°)"].mean()
 my_map = folium.Map(location=[start_lat, start_long], zoom_start=14)
 folium.PolyLine(df_loc[["Latitude (°)", "Longitude (°)"]], color="red", weight=2.5, opacity=1).add_to(my_map)
 
-# Suodatettu kiihtyvyys
 fig1, ax1 = plt.subplots(figsize=(10, 6))
 ax1.plot(df["Time (s)"], df["filter_a_x"], label="Filtered a_x")
 ax1.set_title("Suodatettu kiihtyvyys X-akselilla")
@@ -87,7 +84,7 @@ ax1.set_xlabel("Aika (s)")
 ax1.set_ylabel("Suodatettu_x (m/s²)")
 ax1.legend()
 
-# Tehospektri
+
 N = len(acc)
 dt = df["Time (s)"][1] - df["Time (s)"][0]
 fourier = np.fft.fft(acc, N)
